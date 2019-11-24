324df71f002fbacc8b5fc4c5e9497f91c08c5e25
kishoreIND <kshr5000@gmail.com>
2019-11-24 21:01:00 +0530

    Initial Commit

---
+++ src/main/java/com/employee/portal/EmployeePortalServerApplication.java
@@ -0,0 +1,13 @@
+package com.employee.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePortalServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee/portal/employeeportalserver/EmployeePortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.portal.employeeportalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
